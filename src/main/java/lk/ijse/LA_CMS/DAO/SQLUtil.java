cd0b0948413e7da09943b405ca1a56248e810817
pasindi <pasidingamage@gmail.com>
2024-07-05 13:38:43 +0530

    commited

--- src/main/java/lk/ijse/LA_CMS/DAO/SQLUtil.java
+++ src/main/java/lk/ijse/LA_CMS/DAO/SQLUtil.java
@@ -1,5 +1,23 @@
 package lk.ijse.LA_CMS.DAO;
 
+import lk.ijse.LA_CMS.db.DbConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class SQLUtil {
 
+    public static <T>T execute(String sql, Object... args) throws SQLException {
+        Connection connection=DbConnection.getInstance().getConnection();
+        PreparedStatement preparedStatement=connection.prepareStatement(sql);
+        for (int i = 0; i < args.length; i++) {
+            preparedStatement.setObject((i+1),args[i]);
+        }
+        if (sql.startsWith("SELECT")) {
+            return (T) preparedStatement.executeQuery();
+        }else {
+         return (T) (Boolean) (preparedStatement.executeUpdate()>0);
+        }
+    }
 }
