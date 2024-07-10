e312a4568c9cd28b002ca167d95fd5d7545bd23e
pasindi <pasidingamage@gmail.com>
2024-07-10 11:22:12 +0530

    ChangePasswordDAO

--- src/main/java/lk/ijse/LA_CMS/DAO/custom/ChangePasswordDAO.java
+++ src/main/java/lk/ijse/LA_CMS/DAO/custom/ChangePasswordDAO.java
@@ -2,6 +2,7 @@ package lk.ijse.LA_CMS.DAO.custom;
 
 import lk.ijse.LA_CMS.DAO.CrudDAO;
 import lk.ijse.LA_CMS.DTO.ChangePasswordDTO;
+import lk.ijse.LA_CMS.Entity.User;
 
-public interface ChangePasswordDAO extends CrudDAO<ChangePasswordDTO> {
+public interface ChangePasswordDAO extends CrudDAO<User> {
 }
